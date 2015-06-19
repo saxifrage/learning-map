f16f0b6d16692b14b7e0c9282b23c65873de3eb5
Chad Whitacre <chad@zetaweb.com>
2015-06-19 10:24:02 -0400

    Beef it up with options, collections, and chaining

--- learning-map.js
+++ learning-map.js
@@ -1,3 +1,16 @@
-$.fn.learningMap = function() {
-    this.css('background', 'green');
-};
+(function ($) {
+
+    $.fn.learningMap = function(options) {
+
+        var options = $.extend({
+            color: "green"
+        }, options);
+
+        this.each(function() {
+            $(this).css('background', options.color);
+        });
+
+        return this;
+    };
+
+}(jQuery));
