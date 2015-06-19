e0ed44cf6c23925bb26ff00c4378b4f16683bc8b
Chad Whitacre <chad@zetaweb.com>
2015-06-19 12:30:01 -0400

    Rework for SVG + BFS

--- learning-map.js
+++ learning-map.js
@@ -5,11 +5,18 @@
         var options = $.extend({}, $.fn.learningMap.defaults, options);
 
         this.addClass('learning-map');
-        for (var v in options.dag) {
-            var node = $('<div>');
-            node.addClass('learning-map-node');
-            node.css({top: v*60, left: v*60});
-            this.append(node);
+        this.svg();
+        var svg = this.svg('get');
+
+        var dag = options.dag;
+        var queue = [0];
+
+        while (queue.length) {
+            var i = queue.shift();
+            svg.circle(i*60, i*60, 50, {fill: 'none', stroke: 'yellow', strokeWidth: 3});
+
+            Array.prototype.push.apply(queue, dag[i]);
+            console.log(queue);
         }
 
         return this;
