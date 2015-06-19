4aadee776512a1d1068ffdf6608520fc228a8782
Chad Whitacre <chad@zetaweb.com>
2015-06-19 14:23:22 -0400

    Lay out vertices on a grid

--- learning-map.js
+++ learning-map.js
@@ -12,15 +12,41 @@
         var queue = [0];
         var seen = {};
 
+        /* Set up layout variables.
+         *
+         * We want a side-scrolling layout, with no vertical scrolling. We set
+         * the number of blocks high we want the layout to be, and then compute
+         * a square size for blocks based on the height of the container. Then
+         * we use x and y as the position in our grid.
+         *
+         */
+
+        var nblocks = 5;
+        var canvas_height = this.height();
+        var block_size = canvas_height / nblocks;
+        var half_block = block_size / 2;
+        var x_center, y_center, radius=(block_size * 0.40);
+        var x = 0;                      // start on the left
+        var y = Math.ceil(nblocks / 2); // start in the middle
+
         while (queue.length) {
-            var i = queue.shift();
+            var v = queue.shift();
 
-            if (seen[i]) return;
-            seen[i] = true;
+            if (seen[v]) return;
+            seen[v] = true;
 
-            svg.circle(i*60, i*60, 50, {fill: 'none', stroke: 'yellow', strokeWidth: 3});
+            x = v;
 
-            Array.prototype.push.apply(queue, dag[i]);
+            x_center = (x * block_size) + half_block;
+            y_center = (y * block_size) - half_block;
+
+            svg.circle( x_center
+                      , y_center
+                      , radius
+                      , {fill: 'yellow'}
+                       );
+
+            Array.prototype.push.apply(queue, dag[v]);
         }
 
         return this;
@@ -29,5 +55,4 @@
 }(jQuery));
 
 jQuery.fn.learningMap.defaults = {
-    dag: {}
 };
